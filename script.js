f5552d03a8ea8adff519c36be459784ad4f9eacd
jprekz <jprekz@users.noreply.github.com>
2020-11-10 21:47:32 +0900

    clean

--- script.js
+++ script.js
@@ -32,42 +32,39 @@ onload = async () => {
     const processProgram = create_program(common_vs, process_fs, ['resolution', 'cells', 'rule'], ['position']);
     const renderProgram = create_program(common_vs, render_fs, ['resolution', 'cells'], ['position']);
 
-    // buffer
-    let backBuffer  = create_framebuffer(WIDTH, HEIGHT);
+    let backBuffer = create_framebuffer(WIDTH, HEIGHT);
     let frontBuffer = create_framebuffer(WIDTH, HEIGHT);
 
-    //model
-    let model = {};
-    model.vbo = create_vbo([
-        -1.0, 1.0, 0.0,
-        -1.0, -1.0, 0.0,
-        1.0, 1.0, 0.0,
-        1.0, -1.0, 0.0
-    ]);
-    model.length = 4;
+    const model = {
+        vbo: create_vbo([
+            -1.0, 1.0, 0.0,
+            -1.0, -1.0, 0.0,
+            1.0, 1.0, 0.0,
+            1.0, -1.0, 0.0
+        ]),
+        length: 4,
+    };
 
     gl.bindBuffer(gl.ARRAY_BUFFER, model.vbo);
     gl.enableVertexAttribArray(initProgram.attLocation.position);
     gl.vertexAttribPointer(initProgram.attLocation.position, 3, gl.FLOAT, false, 0, 0);
 
-    (function () {
-        // init
-        gl.useProgram(initProgram.program);
-        // 描画先
-        gl.bindFramebuffer(gl.FRAMEBUFFER, backBuffer.f); // backへ
-        gl.viewport(0, 0, WIDTH, HEIGHT);
-        // バインド
-        gl.uniform2fv(initProgram.uniLocation.resolution, [WIDTH, HEIGHT]);
-        gl.uniform2fv(initProgram.uniLocation.seed, [Math.random(), Math.random()]);
-        // 頂点を描画
-        gl.drawArrays(gl.TRIANGLE_STRIP, 0, model.length);
-    })();
+    // init
+    gl.useProgram(initProgram.program);
+    // 描画先
+    gl.bindFramebuffer(gl.FRAMEBUFFER, backBuffer.framebuffer); // backへ
+    gl.viewport(0, 0, WIDTH, HEIGHT);
+    // バインド
+    gl.uniform2fv(initProgram.uniLocation.resolution, [WIDTH, HEIGHT]);
+    gl.uniform2fv(initProgram.uniLocation.seed, [Math.random(), Math.random()]);
+    // 頂点を描画
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, model.length);
 
     gl.useProgram(processProgram.program);
     gl.uniform1i(processProgram.uniLocation.texture, 0);
     gl.uniform2fv(processProgram.uniLocation.resolution, [WIDTH, HEIGHT]);
     gl.uniform1iv(processProgram.uniLocation.rule, [3, 3, 0, 2, 1, 2, 2, 3, 3]);
-    
+
     gl.useProgram(renderProgram.program);
     gl.uniform1i(renderProgram.uniLocation.texture, 0);
     gl.uniform2fv(renderProgram.uniLocation.resolution, [WIDTH * ZOOM, HEIGHT * ZOOM]);
@@ -76,10 +73,10 @@ onload = async () => {
         // process
         gl.useProgram(processProgram.program);
         // 描画先
-        gl.bindFramebuffer(gl.FRAMEBUFFER, frontBuffer.f); // frontへ
+        gl.bindFramebuffer(gl.FRAMEBUFFER, frontBuffer.framebuffer); // frontへ
         gl.viewport(0, 0, WIDTH, HEIGHT);
         // バインド
-        gl.bindTexture(gl.TEXTURE_2D, backBuffer.t); // backから
+        gl.bindTexture(gl.TEXTURE_2D, backBuffer.texture); // backから
         // 頂点を描画
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, model.length);
 
@@ -89,13 +86,13 @@ onload = async () => {
         gl.bindFramebuffer(gl.FRAMEBUFFER, null); // 画面へ
         gl.viewport(0, 0, cw, ch);
         // バインド
-        gl.bindTexture(gl.TEXTURE_2D, frontBuffer.t); // frontから
-
+        gl.bindTexture(gl.TEXTURE_2D, frontBuffer.texture); // frontから
         // 頂点を描画
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, model.length);
+
         // flush!
         gl.flush();
-
+        // swap!
         [frontBuffer, backBuffer] = [backBuffer, frontBuffer];
 
         if (run) requestAnimationFrame(renderFrame);
@@ -122,34 +119,31 @@ onload = async () => {
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
         gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, fTexture, 0);
-        
+
         gl.bindTexture(gl.TEXTURE_2D, null);
         gl.bindRenderbuffer(gl.RENDERBUFFER, null);
         gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-        
-        return {f : frameBuffer, t : fTexture};
+
+        return { framebuffer: frameBuffer, texture: fTexture };
+    }
+
+    function create_Shader(data, type) {
+        let shader = gl.createShader(type);
+        gl.shaderSource(shader, data);
+        gl.compileShader(shader);
+        if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+            console.error(gl.getShaderInfoLog(shader));
+            return;
+        }
+        return shader;
     }
 
     function create_VertexShader(data) {
-        let vs = gl.createShader(gl.VERTEX_SHADER);
-        gl.shaderSource(vs, data);
-        gl.compileShader(vs);
-        if (!gl.getShaderParameter(vs, gl.COMPILE_STATUS)) {
-            console.error(gl.getShaderInfoLog(vs));
-            return;
-        }
-        return vs;
+        return create_Shader(data, gl.VERTEX_SHADER);
     }
 
     function create_FragmentShader(data) {
-        let fs = gl.createShader(gl.FRAGMENT_SHADER);
-        gl.shaderSource(fs, data);
-        gl.compileShader(fs);
-        if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
-            console.error(gl.getShaderInfoLog(fs));
-            return;
-        }
-        return fs;
+        return create_Shader(data, gl.FRAGMENT_SHADER);
     }
 
     function create_program(vs, fs, uni, att) {
@@ -163,15 +157,15 @@ onload = async () => {
         }
 
         let uniLocation = {};
-        uni.forEach(function(name) {
+        uni.forEach((name) => {
             uniLocation[name] = gl.getUniformLocation(program, name);
         });
 
         let attLocation = {};
-        att.forEach(function(name) {
+        att.forEach((name) => {
             attLocation[name] = gl.getAttribLocation(program, name);
         });
 
-        return {program, uniLocation, attLocation};
+        return { program, uniLocation, attLocation };
     }
 }
